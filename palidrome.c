18ffd5172d02434217797ee30ab2651e9fc65dd0
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-17 03:48:01 +0530

    Update palidrome.c

--- palidrome.c
+++ palidrome.c
@@ -1,19 +1,19 @@
 #include<stdio.h> include<string.h>
 int main(){
-	int i,l,flag;
+	int i,l,flag=0;
 	char s[8];
 	scanf("%s\n",&s[0]);
 	l=strlen(s);
 	for(i=0;i<l;i++){
-		if(s[i]!=s[l-1]){
+		if(s[i]!=s[i-l-1]){
 		flag=1;
 		break;
 		}
 	}
-	if(flag=1){
-		printf("palidrome");
+	if(flag){
+		printf(" not palidrome",s);
 		}else{
-		printf("not palidrime");
+		printf("palidrome",s);
 	}
 return 0;
 }
