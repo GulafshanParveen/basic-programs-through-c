a272df0cf4dc0b68f62efb0b26aab2a5effdda61
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-17 06:02:47 +0530

    Demonstrate the Working of Keyword long

---
+++ long.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a;
+	long l;
+	long int b;
+	double c;
+	long double d;
+	printf("size of int:%zu\n",sizeof(a));
+	printf("size of long:%zu\n",sizeof(l));
+	printf("size of long int:%zu\n",sizeof(b));
+	printf("size of double:%zu\n",sizeof(c));
+	printf("size of long double:%zu\n",sizeof(d));
+return 0;
+}
